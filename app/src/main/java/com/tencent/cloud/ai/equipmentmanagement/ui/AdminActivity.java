984f6142941f43baa9ba973d1c19de91d6e0a6f5
v_hbhong <v_hbhong@tencent.com>
2021-04-27 09:30:13 +0800

    add table at admin

--- app/src/main/java/com/tencent/cloud/ai/equipmentmanagement/ui/AdminActivity.java
+++ app/src/main/java/com/tencent/cloud/ai/equipmentmanagement/ui/AdminActivity.java
@@ -3,14 +3,72 @@ package com.tencent.cloud.ai.equipmentmanagement.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ScrollView;
 
 import com.tencent.cloud.ai.equipmentmanagement.R;
+import com.tencent.cloud.ai.equipmentmanagement.custom.TimeTableView;
+import com.tencent.cloud.ai.equipmentmanagement.model.TimeTableModel;
+import com.tencent.cloud.ai.equipmentmanagement.utils.ScreenshotUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminActivity extends AppCompatActivity {
-
+    private TimeTableView mTimaTableView;
+    private List<TimeTableModel> mList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        mList = new ArrayList<TimeTableModel>();
+        mTimaTableView = (TimeTableView) findViewById(R.id.main_timetable_ly);
+        addList();
+        mTimaTableView.setTimeTable(mList);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    private void addList() {
+        mList.add(new TimeTableModel(0, 1, 2, 1, "8:20", "10:10", "test1",
+                "王老师", "1", "2-13"));
+        mList.add(new TimeTableModel(0, 3, 4, 1, "8:20", "10:10", "test1",
+                "李老师", "2", "2-13"));
+        mList.add(new TimeTableModel(0, 6, 7, 1, "8:20", "10:10", "test1",
+                "王", "3", "2-13"));
+
+
+        mList.add(new TimeTableModel(0, 6, 7, 2, "8:20", "10:10", "test1",
+                "老师1", "4", "2-13"));
+        mList.add(new TimeTableModel(0, 8, 9, 2, "8:20", "10:10", "test1",
+                "老师2", "5", "2-13"));
+
+        mList.add(new TimeTableModel(0, 1, 2, 3, "8:20", "10:10", "test1",
+                "老师3", "6", "2-13"));
+
+        mList.add(new TimeTableModel(0, 6, 7, 3, "8:20", "10:10", "test1",
+                "老师4", "7", "2-13"));
+        mList.add(new TimeTableModel(0, 8, 9, 4, "8:20", "10:10", "test1",
+                "老师5", "9", "2-13"));
+        mList.add(new TimeTableModel(0, 3, 5, 4, "8:20", "10:10", "test1",
+                "老师4", "8", "2-13"));
+        mList.add(new TimeTableModel(0, 6, 8, 5, "8:20", "10:10", "test1",
+                "老师7", "11", "2-13"));
+        mList.add(new TimeTableModel(0, 3, 5, 5, "8:20", "10:10", "test1",
+                "老师6", "10", "2-13"));
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.main_screenshot:
+                break;
+        }
+        return true;
     }
 }
